5757182877096ceb1afbf67719624d36a0781a19
Rahul Wale <152638609+Rahuwale123@users.noreply.github.com>
2023-12-01 22:47:25 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+    const quote = document.getElementById("quote");
+        const author = document.getElementById("author");
+        const setup = document.getElementById("setup");
+        const delivery = document.getElementById("delivery");
 
+        const api_url = "https://api.quotable.io/random";
+        async function getquote(url) {
+            const Response = await fetch(url);
+            let data = await Response.json();
+            quote.innerHTML = data.content;
+            author.innerHTML = data.author;
+
+        }
+        const api_urls = "https://v2.jokeapi.dev/joke/Any";
+        async function getnews(url) {
+            const Response = await fetch(url);
+            let data = await Response.json();
+        setup.innerHTML = data.setup;
+        delivery.innerHTML = data.delivery;
+
+        }
+        getnews(api_urls);
+        getquote(api_url);
